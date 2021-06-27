0c6ace130388f0c872f53f85c5818ee3721ea717
kmrshubham123 <kmrshubham123@gmail.com>
2021-06-27 21:42:29 +0530

    [Kumar Shubham]Refactor

--- MoodAnalyzer/MoodAnalyzer/Program.cs
+++ MoodAnalyzer/MoodAnalyzer/Program.cs
@@ -15,10 +15,6 @@ namespace MoodAnalyzer
             //UC2,3:-
             MoodAnalyser moodAnalyser = new MoodAnalyser("");
             Console.WriteLine("Given Null Message:- "+ moodAnalyser.AnalyseMood());
-<<<<<<< HEAD
-           
-=======
->>>>>>> UC6-ReflectionToInvokeMethod
         }
     }
 }
